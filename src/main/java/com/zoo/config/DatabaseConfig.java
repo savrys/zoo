98da44ef3cd75d0fefeaa3f16f56142542f6cf77
Савин Арсений <savars328@gmail.com>
2026-09-21 11:58:24 +0300

    четные попытки подключить бд

--- src/main/java/com/zoo/config/DatabaseConfig.java
+++ src/main/java/com/zoo/config/DatabaseConfig.java
@@ -4,5 +4,5 @@ public class DatabaseConfig {
     // ЗАМЕНИТЕ на свои данные
     public static final String URL = "jdbc:postgresql://localhost:5432/zoo_db";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "p328555SDAp";
+    public static final String PASSWORD = "12345";
 }
