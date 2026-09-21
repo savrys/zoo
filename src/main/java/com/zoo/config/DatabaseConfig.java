1d9b694b208e4c11377eb44d7af74fef8b812ed9
Савин Арсений <savars328@gmail.com>
2026-09-21 12:05:50 +0300

    небольшие правки

--- src/main/java/com/zoo/config/DatabaseConfig.java
+++ src/main/java/com/zoo/config/DatabaseConfig.java
@@ -2,7 +2,7 @@ package com.zoo.config;
 
 public class DatabaseConfig {
     // ЗАМЕНИТЕ на свои данные
-    public static final String URL = "jdbc:postgresql://localhost:5432/zoo_db";
+    public static final String URL = "jdbc:postgresql://localhost:5434/zoo_db";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "12345";
+    public static final String PASSWORD = "p328555SDAp";
 }
